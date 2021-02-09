edce38565e67155c1e4638e68628313e521192b5
Christina Vicente <christina.vicente@hcl.com>
2021-02-09 15:54:29 -0500

    inital commit

---
+++ src/main/java/com/hcl/display_feedback/DisplayFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.display_feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplayFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisplayFeedbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/display_feedback/DisplayFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.display_feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
